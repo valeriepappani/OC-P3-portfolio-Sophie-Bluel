1745c5a64abce4b61e3e7f433f82a5a9adcf4f30
Valerie Pappani <v.pappani@smallstories.fr>
2023-08-23 10:05:44 +0200

    ajout informations de connexion + message d'erreur

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,48 +1,79 @@
- function ajoutListenerLogin() {
+function ajoutListenerLogin() {
     const formulaireLogin = document.querySelector(".formulaire-login");
-    formulaireLogin.addEventListener("submit", function (event) {
+    formulaireLogin.addEventListener("submit", async function (event) {
+
         event.preventDefault();
+
         // Création de l’objet de connexion
         const connexion = {
             email: event.target.querySelector("[name=email]").value,
             password: event.target.querySelector("[name=password]").value,
         };
-        console.log(connexion);
-        // Création de la charge utile au format JSON
-        const chargeUtile = JSON.stringify(connexion);
 
-        fetch('http://localhost:5678/api/users/login', {
-            method: "POST",
-            headers: { 
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-             },
-            body: chargeUtile
+        let statutConnexion = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(connexion)
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-    });
+        // .then(response => response.json())
+        // .then(data => {
+        //     window.localStorage.setItem("token", data.token);
+        //     window.localStorage.setItem("userId", data.userId);
+        // })
+        const logResponse = await statutConnexion.json();
+        window.localStorage.setItem("token", logResponse.token); //Stockage du token dans le localStorage
+        window.localStorage.setItem("userId", logResponse.userId);//Stockage de l'userId dans le localStorage
+
+        const logStatus = statutConnexion.status;
+        console.log(`le statut de connexion est ${logStatus}`)
+
+        //Message d'erreur et redirection
+        if (logStatus === 200) {
+            // console.log('L\'utilisateur est validé, nous pouvons rediriger la page');
+            window.location.href = '/FrontEnd/index.html'; //redirection vers la homepage 
+        } else if (logStatus === 404 || logStatus === 401) {
+            // console.log("Utilisateur introuvable ! Merci de vérifier votre adresse email et/ou votre mot de passe")
+            const erreur = document.querySelector("form");
+            const messageErreur = document.createElement("p");
+            messageErreur.innerHTML = `Utilisateur introuvable ! </br> Merci de vérifier votre adresse email et/ou votre mot de passe`;
+            messageErreur.classList.add("message-erreur");
+            erreur.appendChild(messageErreur);
+        }
+    })
 }
 
+ajoutListenerLogin()
 
-ajoutListenerLogin() 
 
 
-// Récupération des informations de connexion donné par l'utilisateur
-// let form = document.querySelector("form");
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
+const userId = localStorage.getItem("userId");
+console.log(`l'userId est ${userId}`)
 
-//     let baliseEmail = document.getElementById("email");
-//     let email = baliseEmail.value;
-//     console.log(email);
+//Bouton Login/logout
+if (userId != null) {
+    console.log("L'utilisateur est connecté")
+    const btnLog = document.querySelector('.btn-login');
+    btnLog.innerText = "logout"
+} 
 
-//     let balisePassword = document.getElementById("password");
-//     let password = balisePassword.value;
-//     console.log(password);
-// })
+
+function logout () {
+    const logout = document.querySelector(".btn-login");
+    logout.addEventListener("click", async function (event) {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("token");
+        console.log(localStorage)
+        const btnLog = document.querySelector('.btn-login');
+        btnLog.innerText = "login"
+        console.log("L\'utilisateur n'est plus connecté")
+    
+    })
+}
+
+logout();
 
 
 /*****Mise en forme erreur de connexion*****/
